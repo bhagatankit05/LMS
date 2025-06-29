16407e1c482dfaae4f9d06d8c3aeebbdd1f06f0d
bhagatankit05 <bhagatankit82@gmail.com>
2025-06-29 16:38:10 +0530

    add components for educators and students

---
+++ client/src/components/students/SearchBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SearchBar = () => {
+  return (
+    <div>
+      <h1>search Bar</h1>
+    </div>
+  )
+}
+
+export default SearchBar
---
+++ client/src/components/students/TestiMonialSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TestiMonialSection = () => {
+  return (
+    <div className="">
+      <h1>TestiMonial Section</h1>
+    </div>
+  )
+}
+
+export default TestiMonialSection
