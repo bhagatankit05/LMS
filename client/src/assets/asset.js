8ab7b3587ac4b3f610f44a521729361cae39ac9a
bhagatankit05 <bhagatankit82@gmail.com>
2025-07-02 23:48:32 +0530

    Add comapanies

--- client/src/assets/asset.js
+++ client/src/assets/asset.js
@@ -1,10 +1,18 @@
 import logo1 from './logo1.svg';
 import user from './user.svg';
 import search_icon from './search_icon.svg';
+import Capgemini_Logo from './Capgemini_Logo.png';
+import infosys from './infosys.jpg';
+import tcs_icon from  './tcs_icon.png'
+import devsync from './devsync.png'
 
 
 export const assets = {
   logo1: logo1,
   user: user,
   search_icon: search_icon,
+  Capgemini_Logo:Capgemini_Logo,
+  infosys: infosys,
+  tcs_icon:tcs_icon,
+  devsync:devsync,
 }
--- client/src/components/students/Companies.jsx
+++ client/src/components/students/Companies.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { assets } from '../../assets/asset'
 
 const Companies = () => {
   return (
-    <div>
-      <h1>Companies</h1>
+    <div className='pt-16'>
+        <p className='text-base text-gray-500'>Empowering learners from</p>
+        <div className='flex flex-wrap items-center justify-center gap-6 mg:gap-16 md:mt-10 mt-5'>
+          <img src={assets.devsync} alt="" className='w-20 md:w-28'/>
+          <img src={assets.Capgemini_Logo} alt="" className='bg-white w-20 md:w-28'/>
+          <img src={assets.infosys} alt="" className='w-20 md:w-28'/>
+          <img src={assets.tcs_icon} alt="" className='w-20 md:w-28'/>
+          
+
+
+
+        </div>
     </div>
   )
 }
