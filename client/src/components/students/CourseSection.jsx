901eb547ecc3f31990700f2529c503e139662be0
bhagatankit05 <bhagatankit82@gmail.com>
2025-07-03 19:47:45 +0530

    add course section

--- client/src/components/students/CourseSection.jsx
+++ client/src/components/students/CourseSection.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const CourseSection = () => {
   return (
-    <div>
-      <h1>Course Section</h1>
+    <div className='py-16 md:px-40 px-8'>
+        <h2 className='text-3xl font-medium text-gray-800'>Learn from the best</h2>
+        <p className='text-sm md:text-base text-gray-500 mt-3'>Browse our top-performing courses across diverse fields. From programming and design to business and wellness, each course is thoughtfully developed to ensure impactful learning.</p>
+
+
+
+        <Link to={'/course-list'} onClick={()=>scrollTo(0,0)} className='text-gray-500 border border-gray-500/30 px-10 py-3 rounded'>Show all courses </Link>
     </div>
   )
 }
