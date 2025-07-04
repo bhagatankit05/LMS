edbe359e6ba44fb3eb221bf88fc0d499c6bc4f42
bhagatankit05 <bhagatankit82@gmail.com>
2025-07-04 19:42:52 +0530

    add navigate to home page on logo

--- client/src/components/students/Navbar.jsx
+++ client/src/components/students/Navbar.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { assets } from '../../assets/asset'
 import { Link, useLocation } from 'react-router-dom'
 import { useClerk, UserButton, useUser } from '@clerk/clerk-react'
+import { AppContext } from '../../context/AppContext'
 
 const Navbar = () => {
   // This is the Navbar component for the student side of the application.
+
+  const {navigate} = useContext(AppContext); 
   const isCourseListPage = location.pathname.includes('/course-list');
   const { openSignIn } = useClerk();
   const { user } = useUser();
@@ -12,7 +15,7 @@ const Navbar = () => {
 
   return (
     <div className={`flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-200 py-4 ${isCourseListPage ? 'bg-white' : 'bg-orange-50'}`}>
-      <img src={assets.logo1} alt="LOGO" className='w-28 lg:w-32 cursor-pointer rounded' />
+      <img onClick={()=>navigate('/')} src={assets.logo1} alt="LOGO" className='w-28 lg:w-32 cursor-pointer rounded' />
 
       <div className='hidden md:flex items-center gap-5 text-gray-500 '>
         {/* For desktop screen*/}
--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,10 +1,13 @@
 import { createContext, useEffect, useState } from "react";
 import { dummyCourses } from "../assets/asset";
+import { useNavigate } from "react-router-dom";
 
 export const AppContext = createContext();
 
 export const AppContextProvider = (props) => {
     const currency = import.meta.env.VITE_CURRENCY
+
+    const navigate = useNavigate()
 
     const[allCourses , setAllCourses]=useState([]);
 
@@ -19,7 +22,7 @@ export const AppContextProvider = (props) => {
 
   const contextValue = {
     // Define any state or functions you want to provide to the context
-    currency,allCourses
+    currency,allCourses,navigate,
 
   };
 
