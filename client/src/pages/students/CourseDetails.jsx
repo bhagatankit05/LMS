53ce75706e6006285f1c23dd53539e59bb3ec4d0
bhagatankit05 <bhagatankit82@gmail.com>
2025-07-15 22:58:35 +0530

    added toggle functionality for Chapter content

--- client/src/pages/students/CourseDetails.jsx
+++ client/src/pages/students/CourseDetails.jsx
@@ -8,6 +8,8 @@ import humanizeDuration from 'humanize-duration';
 const CourseDetails = () => {
   const { id } = useParams();
   const [courseData, setCourseData] = useState(null);
+  const [openSections, setOpenSections] = useState({});
+
   const { allCourses,calculateRating,calculateNoOfLectures,calculateCourseDuration,calculateChapterTime} = useContext(AppContext);
 
   const fetchCourseData = () => {
@@ -18,6 +20,14 @@ const CourseDetails = () => {
   useEffect(() => {
     fetchCourseData();
   }, []);
+
+  const toggleSection = (index)=>{
+    setOpenSections((prev)=>(
+      { ...prev,
+        [index]: !prev[index]
+      }
+    ))
+  }
 
   return courseData ? (
     <>
@@ -55,14 +65,15 @@ const CourseDetails = () => {
                       <div className='pt-5'>
                           {courseData.courseContent.map((chapter,index)=>(
                             <div key={index} className='border border-gray-300 bg-white mb-2 rounded'>
-                                <div className='flex items-center justify-between px-4 py-3 cursor-pointer select-none'>
+                                <div className='flex items-center justify-between px-4 py-3 cursor-pointer select-none' onClick={()=> toggleSection(index)}>
                                   <div className='flex items-center gap-2'>
                                     <img src={assets.down_arrow_head} alt="" className='w-5 h-5'/>
                                     <p className='font-medium md:text-base text-sm '>{chapter.chapterTitle}</p>
                                   </div>
                                   <p className='text-sm md:text-default'>{chapter.chapterContent.length} lectures - {calculateChapterTime(chapter)}</p>
                                 </div>
-                                <div className='overflow-hidden transition-all duration-300 max-h-96'>
+
+                                <div className={`overflow-hidden transition-all duration-300 ${openSections[index] ? 'max-h-96' : ' max-h-0'} `}>
                                   <ul className='list-disc md:pl-10 pl-4 pr-4 py-2 text-gray-600 border-t border-gray-300'>
                                     {chapter.chapterContent.map((lecture, i)=>(
                                       <li key={i} className='flex items-start gap-2 py-1'>
