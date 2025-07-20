1ed2af6b1ab35c109c3076da45673896fbf2efcc
bhagatankit05 <bhagatankit82@gmail.com>
2025-07-20 23:44:22 +0530

    make table of enrolled courses

--- client/src/pages/students/MyEnrollment.jsx
+++ client/src/pages/students/MyEnrollment.jsx
@@ -1,10 +1,51 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AppContext } from '../../context/AppContext'
+import e from 'cors';
 
 const MyEnrollment = () => {
+  const { enrolledCourses, calculateCourseDuration } = useContext(AppContext);
   return (
-    <div>
-      <h1>my Enrollment</h1>
-    </div>
+    <>
+      <div className='md:px-36 px-8 pt-10'>
+        <h1 className='text-2xl font-semibold'>My Enrollments</h1>
+        <table className='md:table-auto table-fixed w-full overflow-hidden border mt-10'>
+          <thead className='text-gray-900 border-b border-gray-500/20 text-sm text-left max-sm:hidden'>
+            <tr>
+              <th className='px-4 py-3 font-semibold truncate'>Course</th>
+              <th className='px-4 py-3 font-semibold truncate'>Duration</th>
+              <th className='px-4 py-3 font-semibold truncate'>Completed</th>
+              <th className='px-4 py-3 font-semibold truncate'>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {enrolledCourses.map((course, index) => (
+              <tr key={index}>
+                <td>
+                  <img src={course.courseThumbnail} alt="course-thumbnail" className='w-14 sm:w-24 md:w-28' />
+                  <div>
+                    <p>{course.courseTitle}</p>
+                  </div>
+                </td>
+
+                <td>
+
+                  {calculateCourseDuration(course)}
+                </td>
+                <td>
+                  4 / 10 <span>Lectures</span>
+                </td>
+
+                <td>
+                  <button>
+                    On Going
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
   )
 }
 
