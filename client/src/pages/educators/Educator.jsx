d89c55854d3b000e8b5dde4d7ccee65bf974e64f
bhagatankit05 <bhagatankit82@gmail.com>
2025-07-22 21:21:29 +0530

    Refactor Navbar and Educator components for improved layout and user integration

--- client/src/pages/educators/Educator.jsx
+++ client/src/pages/educators/Educator.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Navbar from '../../components/educators/Navbar'
 
 const Educator = () => {
   return (
-    <div>
-      <h1>Educator page</h1>
+    <div className='text-default min-h-screen bg-white'>
+      <Navbar/> 
       <div>
             {<Outlet />  }    {/* This is where the nested routes will be rendered */}
       </div>
