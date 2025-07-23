fd568fbcf88ec9f3aab3f035dda65a0761df5639
bhagatankit05 <bhagatankit82@gmail.com>
2025-07-23 06:37:30 +0530

    added SideBar in Educator section

--- client/src/pages/educators/Educator.jsx
+++ client/src/pages/educators/Educator.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from '../../components/educators/Navbar'
+import Sidebar from '../../components/educators/Sidebar'
 
 const Educator = () => {
   return (
-    <div className='text-default min-h-screen bg-white'>
+    <div className='text-default min-h-screen bg-white '>
       <Navbar/> 
-      <div>
+      <div className='flex'>
+        <Sidebar/>
+          <div className='flex-1'>
             {<Outlet />  }    {/* This is where the nested routes will be rendered */}
+            </div>
       </div>
     </div>
   )
