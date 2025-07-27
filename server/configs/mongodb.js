52ab21b69c6422e974d2197d986efc4bbc5f3e27
bhagatankit05 <bhagatankit82@gmail.com>
2025-07-27 16:59:24 +0530

    updated the mongodb.js

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -1,10 +1,16 @@
 import mongoose from "mongoose";
 
-//Connect to the MongoDB database.
-const connectDB = async ()=>{
-    mongoose.connection.on('connected' , ()=> console.log('Database Connected!'))
-    
-    await mongoose.connect(`${process.env.MONGODB_URI}/GyaanSetu`) //GyaanSetu is the name of the Database.
-}
+const connectDB = async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/GyaanSetu`, { //GyaanSetu is the name of database.
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("Database Connected to GyaanSetu!");
+  } catch (error) {
+    console.error("MongoDB connection error:", error.message);
+  }
+};
 
 export default connectDB;
