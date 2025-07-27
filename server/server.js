71010d118b23fd5d7fe8e502e29883864c7b9fbd
bhagatankit05 <bhagatankit82@gmail.com>
2025-07-27 16:16:48 +0530

    updated server.js

--- server/server.js
+++ server/server.js
@@ -10,13 +10,15 @@ const app = express();
 //Connect to DataBase.
 await connectDB()
 
+app.use(express.json()); // Global JSON parsing
+
 //middleware
 app.use(cors());
 
 //Routes
 app.get('/',(req,res)=> res.send("API is Working"))
 
-app.post('/clerk',  express.json(), clerkWebhooks)
+app.post('/clerk', clerkWebhooks)
 
 //port
 const PORT = process.env.PORT || 5000
