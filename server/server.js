5e84af016c2acbc5dd45bccb769034840a4b54c5
bhagatankit05 <bhagatankit82@gmail.com>
2025-07-29 19:23:54 +0530

    added courseRoute in server.js file

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ import { clerkWebhooks } from './controllers/webhooks.js';
 import educatorRouter from './routes/educatorRoutes.js';
 import { clerkMiddleware } from '@clerk/express';
 import connectCloudinary from './configs/cloudinary.js';
+import courseRouter from './routes/courseRoute.js';
 
 //Initialize Express
 const app = express();
@@ -27,6 +28,7 @@ app.get('/',(req,res)=> res.send("API is Working"))
 app.post('/clerk', clerkWebhooks)
 
 app.use('/api/educator',educatorRouter);
+app.use('api/course',courseRouter);
 
 //port
 const PORT = process.env.PORT || 5000
