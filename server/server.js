3f2dab05c3db69829628225b59a1e01f7da8ad2f
bhagatankit05 <bhagatankit82@gmail.com>
2025-07-30 22:15:10 +0530

    feat: add Stripe webhook handling for payment events

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './configs/mongodb.js';
-import { clerkWebhooks } from './controllers/webhooks.js';
+import { clerkWebhooks, stripeWebHooks } from './controllers/webhooks.js';
 import educatorRouter from './routes/educatorRoutes.js';
 import { clerkMiddleware } from '@clerk/express';
 import connectCloudinary from './configs/cloudinary.js';
@@ -31,6 +31,7 @@ app.post('/clerk', clerkWebhooks)
 app.use('/api/educator',educatorRouter);
 app.use('api/course',courseRouter);
 app.use('api/user',userRouter)
+app.post('/stripe',express.raw({type:'application/json'}),stripeWebHooks)
 
 //port
 const PORT = process.env.PORT || 5000
