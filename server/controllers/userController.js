45d5082fbb34af5213aaa965529a19dace0fea2e
bhagatankit05 <bhagatankit82@gmail.com>
2025-07-30 20:38:59 +0530

    implemented user Enrolled Courses with lecture link

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -16,3 +16,16 @@ export const getUserData = async()=>{
             res.json({success:false,message: error.message})
     }
 }
+
+//Users Enrolled Courses With Lecture Links.
+export const userEnrolledCourses = async (req,res)=>{
+    try {
+        const userId = req.auth.userId
+        const userData = await User.findById(userId).populate('enrolledCourses');
+
+        res.json({success:true, enrolledCourses: userData.enrolledCourses})
+
+    } catch (error) {
+        res.json({success:false , message: error.message})
+    }
+}
