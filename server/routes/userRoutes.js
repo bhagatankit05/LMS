35ed7b79aa6f1b982ab4652eee4f08e663230d76
bhagatankit05 <bhagatankit82@gmail.com>
2025-07-31 15:13:13 +0530

    added route of  add rating

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { getUserCourseProgress, getUserData, purchaseCourse, updateUserCourseProgress, userEnrolledCourses } from '../controllers/userController.js'
+import { addUserRating, getUserCourseProgress, getUserData, purchaseCourse, updateUserCourseProgress, userEnrolledCourses } from '../controllers/userController.js'
 
 const userRouter = express.Router()
 userRouter.get('/data', getUserData);
@@ -8,5 +8,6 @@ userRouter.post("/purchase", purchaseCourse)
 
 userRouter.post('/update-course-progress', updateUserCourseProgress)
 userRouter.post('/get-course-progress', getUserCourseProgress)
+userRouter.post('/add-ratings', addUserRating)
 
 export default userRouter;
